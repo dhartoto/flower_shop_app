5ae5c935bd28533569aa4d08dd969776cc8d05c8
Ardito Hartoto <dhartoto@gmail.com>
2015-07-28 16:00:46 +1000

    Add #run to FlowerShop
    Implemented order fill. Validations of user input not implemented
    yet. OrderFiller class and .fill added for mock
    to work in test. 2 tests passing

--- lib/flower_shop.rb
+++ lib/flower_shop.rb
@@ -1,4 +1,5 @@
 require 'products'
+require 'order_filler'
 
 class FlowerShop
   attr_reader   :products
@@ -9,10 +10,18 @@ class FlowerShop
     @products = Products.all
   end
 
+  def run
+    puts 'Welcome to the Flower Shop App.'
+    puts display # display instructions
+    order_filler = OrderFiller.fill(self)
+    self.display = order_filler.message # breakdown or error message
+    puts display # display response from order filler
+  end
+
   private
 
   def file_upload_instructions
-    "Please save Order CSV file into the 'uploads' folder then enter 1"\
-      " to create invoice, or 2. to exit."
+    "Please save Order CSV file into the 'uploads' folder then enter"\
+      " '1' to calculate costs and bundle breakdown, or '2' to exit."
   end
 end
