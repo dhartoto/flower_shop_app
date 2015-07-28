42e778284cc2b40fe29dc88e58c9f73b27c4c79d
Ardito Hartoto <dhartoto@gmail.com>
2015-07-28 20:40:21 +1000

    Add failing tests for Catalogue #find
    3 specs added for #find to return a hash keys 'name' and 'bundles'
    3 tests failing for #find
    
    Also DRYed up tests for .create
    All tests passing for .create

--- spec/lib/catalogue_spec.rb
+++ spec/lib/catalogue_spec.rb
@@ -2,45 +2,51 @@ require 'spec_helper'
 require 'catalogue'
 
 describe Catalogue do
-  it { should be_an_instance_of(Catalogue) }
-  describe '.create' do
-    let(:yaml_mock) do
-      { "R12" => {
-        "name" => "Roses",
-        "bundles" => {
-          5 => 6.99,
-          10 => 12.99 }
-        },
-        "L09" => {
-          "name" => "Lilies",
-          "bundles" => {
-            3 => 16.99,
-            6 => 16.95,
-            9 => 24.95 }
-          }
-      }
-    end
 
-    before do
-      allow(YAML).to receive(:load_file) { yaml_mock }
-    end
+  let(:yaml_mock) do
+    { "R12" => {
+      "name" => "Roses",
+      "bundles" => {
+        5 => 6.99,
+        10 => 12.99 }
+      }
+    }
+  end
+
+  before do
+    allow(YAML).to receive(:load_file) { yaml_mock }
+  end
+
+  let(:catalogue) { Catalogue.create }
+
+  describe '.create' do
+    let(:product) { catalogue.products['R12'] }
 
     it 'responds to products' do
-      catalogue = Catalogue.create
       expect(catalogue).to respond_to :products
     end
     it 'assigns a hash of products' do
-      catalogue = Catalogue.create
-      expect(catalogue.products['R12']).to be_an_instance_of(Hash)
+      expect(product).to be_an_instance_of(Hash)
     end
     it 'assigns a hash of products with name' do
-      catalogue = Catalogue.create
-      product = catalogue.products['R12']
       expect(product['name']).to eq('Roses')
     end
     it 'assigns a hash of products with name' do
-      catalogue = Catalogue.create
-      product = catalogue.products['R12']
+      expected_hash = { 5 => 6.99, 10 => 12.99 }
+      expect(product['bundles']).to eq(expected_hash)
+    end
+  end
+
+  describe '#find' do
+    let(:product) { catalogue.find('R12') }
+
+    it 'returns a hash' do
+      expect(catalogue.find('R12')).to eq(yaml_mock)
+    end
+    it 'returns hash with name' do
+      expect(product['name']).to eq('Roses')
+    end
+    it 'returns hash with bundles' do
       expected_hash = { 5 => 6.99, 10 => 12.99 }
       expect(product['bundles']).to eq(expected_hash)
     end
