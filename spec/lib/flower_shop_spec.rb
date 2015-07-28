0b4266f4f038409350f3aa6c1369051052ad4e3b
Ardito Hartoto <dhartoto@gmail.com>
2015-07-28 16:07:46 +1000

    Add failing tests to FlowerShop
    Add failing tests for user input 2, user choose to exit app.
    Test that OrderFiller is not called and that exit message
    is displayed. 2 tests failing.

--- spec/lib/flower_shop_spec.rb
+++ spec/lib/flower_shop_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 require 'flower_shop'
-require 'products'
 
 describe FlowerShop do
   describe '.new' do
@@ -24,12 +23,13 @@ describe FlowerShop do
     let(:order_filler) { instance_double('OrderFiller', message: 'some message') }
 
     before do
-      allow(app).to receive(:gets).with('1')
       allow(Products).to receive(:all)
       allow(OrderFiller).to receive(:fill) { order_filler }
     end
 
     context 'when user enters 1 to calculate costs and bundle' do
+      before { allow(app).to receive(:gets).with('1') }
+
       it 'fills order' do
         expect(OrderFiller).to receive(:fill)
         app.run
@@ -37,6 +37,19 @@ describe FlowerShop do
       it 'displays message to user' do
         app.run
         expect(app.display).to eq('some message')
+      end
+    end
+
+    context 'when user enters 2 to exit program' do
+      before { allow(app).to receive(:gets).with('2') }
+      
+      it 'does not fill order' do
+        expect(OrderFiller).not_to receive(:fill)
+        app.run
+      end
+      it 'displays exit message to user' do
+        app.run
+        expect(app.display).to eq("Exiting the Flower Shop")
       end
     end
   end
