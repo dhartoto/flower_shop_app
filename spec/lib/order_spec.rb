fdf094a8f7fef9ab71e8c20edabbe01bc7d34075
Ardito Hartoto <dhartoto@gmail.com>
2015-07-29 13:17:34 +1000

    Update spec for initializing Order
    Added failing test for assignment of empty array to items on
    instantiation. 1 test passing and 1 test failing.

--- spec/lib/order_spec.rb
+++ spec/lib/order_spec.rb
@@ -8,5 +8,13 @@ describe Order do
       order = Order.new(Catalogue.new)
       expect(order.catalogue).to be_an_instance_of(Catalogue)
     end
+    it 'assigns empty array to items' do
+      order = Order.new('catalogue')
+      expect(order.items).to eq([])
+    end
+  end
+
+  describe '#create' do
+
   end
 end
