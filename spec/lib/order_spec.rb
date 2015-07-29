b7c6aaa33c2a33b01a9a61c378336d9b88595e14
Ardito Hartoto <dhartoto@gmail.com>
2015-07-29 15:10:26 +1000

    Update Order with file invalid spec
    Verify that Items are not created and that error message from
    file is assigned to Order's error_message attribute.
    
    2 tests failing.

--- spec/lib/order_spec.rb
+++ spec/lib/order_spec.rb
@@ -28,7 +28,6 @@ describe Order do
 
     context 'when file is valid' do
       let(:content) { [["10 R12"], ["15 L09"], ["13 T58"]] }
-
       let(:file) do
         instance_double('Customer::File', valid?: true, content: content)
       end
@@ -42,6 +41,23 @@ describe Order do
       it 'appends item to items attribute' do
         order.create
         expect(order.items.count).to eq(3)
+      end
+    end
+
+    context 'when file is invalid' do
+      let(:file) do
+        instance_double('Customer::File', valid?: false, error_message: 'error')
+      end
+
+      before { allow(Customer::File).to receive(:retreive) { file } }
+
+      it 'does not create items' do
+        expect(Item).not_to receive(:create)
+        order.create
+      end
+      it 'assigns error message' do
+        order.create
+        expect(order.error_message).to eq('error')
       end
     end
   end
