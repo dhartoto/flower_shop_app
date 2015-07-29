51d44d02282646138825304ee186047d0e4355ef
Ardito Hartoto <dhartoto@gmail.com>
2015-07-29 14:23:18 +1000

    Update Order spec
    3 failing tests added to verify if customer order file is
    received, and when it is valid, create items and append it to
    Order's items attribute.

--- spec/lib/order_spec.rb
+++ spec/lib/order_spec.rb
@@ -3,18 +3,43 @@ require 'order'
 require 'catalogue'
 
 describe Order do
+  let(:catalogue) { Catalogue.new }
+
   describe '.new' do
     it 'assigns an instance of catalogue' do
-      order = Order.new(Catalogue.new)
+      order = Order.new(catalogue)
       expect(order.catalogue).to be_an_instance_of(Catalogue)
     end
     it 'assigns empty array to items' do
-      order = Order.new('catalogue')
+      order = Order.new(catalogue)
       expect(order.items).to eq([])
     end
   end
 
   describe '#create' do
+    let(:order) { Order.new(catalogue) }
 
+    it 'retreives customer order file' do
+      expect(Customer::File).to receive(:retreive)
+    end
+
+    context 'when file is valid' do
+      let(:content) { [["10 R12"], ["15 L09"], ["13 T58"]] }
+
+      let(:file) do
+        instance_double('Customer::File', valid?: true, content: content)
+      end
+
+      before { allow(Customer::File).to receive(:retreive) { file } }
+
+      it 'creates items' do
+        expect(Item).to receive(:create).exactly(3).times
+        order.create
+      end
+      it 'appends item to items attribute' do
+        order.create
+        expect(order.items.count).to eq(3)
+      end
+    end
   end
 end
