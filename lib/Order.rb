952964f6038da619609af2b23bbce278356753a9
Ardito Hartoto <dhartoto@gmail.com>
2015-07-29 13:19:09 +1000

    Add items attribute to Order
    all tests passing for Order initialization.

--- lib/Order.rb
+++ lib/Order.rb
@@ -1,9 +1,10 @@
 class Order
   attr_reader   :catalogue
-  attr_accessor :error_message
+  attr_accessor :error_message, :items
 
   def initialize(catalogue)
     @catalogue = catalogue
+    @items     = Array.new
   end
 
   def valid?
