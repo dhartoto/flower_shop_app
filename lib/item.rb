94535e72f3d4149cb6574e062b03d3d0996c3694
Ardito Hartoto <dhartoto@gmail.com>
2015-07-29 16:28:20 +1000

    Complete implementation of Item with .create
    Added code and quantity attributes. Added data conversion of
    argument string, which is a line from the order.csv file.
    3 tests passing for Item.

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,15 @@
 class Item
   def self.create(line)
+    line = line.split(' ')
+    code = line[1]
+    quantity = line[0].to_i
+    new(code: code, quantity: quantity)
+  end
+
+  attr_reader :code, :quantity
+
+  def initialize(options={})
+    @code     = options[:code]
+    @quantity = options[:quantity]
   end
 end
