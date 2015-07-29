9d32d3d623402e3d22e4285506cf9dc8ec001a35
Ardito Hartoto <dhartoto@gmail.com>
2015-07-30 09:09:44 +1000

    Update Customer::File.get
    Added parsing of order.csv file and assignment of
    file content to 'content' attribute. Constructor
    also added. 1 test passing for Customer::File
    
    order.csv sample file was also added to public/uploads

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,8 +1,20 @@
+require 'csv'
+
 module Customer
 
   class File
+    FILE_NAME = 'order.csv'
+    FULL_PATH = "public/uploads/#{FILE_NAME}"
+
+    def self.get
+      file = CSV.read(FULL_PATH)
+      new(content: file)
+    end
+
     attr_accessor :error_message, :content
-    def self.retreive
+
+    def initialize(options={})
+      @content = options[:content]
     end
 
     def valid?
