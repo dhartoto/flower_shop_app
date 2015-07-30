d14821881b1ede3123aeefb8c4256a4633849159
Ardito Hartoto <dhartoto@gmail.com>
2015-07-30 20:56:41 +1000

    Add failing tests to Packer
    tests added to verify that Packs are created
    2 tests failing.

--- spec/lib/packer_spec.rb
+++ spec/lib/packer_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper'
 require 'packer'
-require 'order'
 
 describe Packer do
-  let(:order) { Order.new }
+  let(:items) { ['item_1', 'item_2'] }
+  let(:order) { OpenStruct.new(items: items) }
 
   describe '.pack' do
     it 'assigns order to packer' do
       package = Packer.pack(order)
       expect(package.order).to eq(order)
     end
-    it 'creates one pack if there is one order'
-    it 'creates many packs if there are mnay orders'
+    it 'creates one pack if there is one order' do
+      expect(Pack).to receive(:create)
+      Packer.pack(order)
+    end
+    it 'creates 3 packs if there are 3 orders' do
+      expect(Pack).to receive(:create).exactly(3).times
+      Packer.pack(order)
+    end
   end
 end
