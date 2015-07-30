d276b2c1352b0f037e91f8be01bf7596adbba132
Ardito Hartoto <dhartoto@gmail.com>
2015-07-30 21:35:16 +1000

    Add product code and quantity to Pack
    2 tests passing.

--- lib/pack.rb
+++ lib/pack.rb
@@ -1,4 +1,12 @@
 class Pack
   def self.create(item)
+    new(code: item.code, quantity: item.quantity)
+  end
+
+  attr_reader :code, :quantity
+
+  def initialize(options={})
+    @code = options[:code]
+    @quantity = options[:quantity]
   end
 end
