06586d7adff7ec16d2273cde2b27c3767dc866ca
Ardito Hartoto <dhartoto@gmail.com>
2015-08-02 19:56:51 +1000

    Add failing tests for Invoicer.create
    Tests added to verify assignment of package, calculation of total
    value, and order breakdown output.
    3 tests failing.

---
+++ spec/lib/invoicer_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+require 'invoicer'
+require 'ostruct'
+require 'pack'
+require 'catalogue'
+require 'pry'
+
+describe Invoicer do
+
+  describe '.create' do
+    context 'when single product order' do
+      let(:catalogue) { Catalogue.create }
+
+      let(:packs) do
+        [Pack.new(
+          code: 'R12',
+          total_quantity: 15,
+          bundles: { 5 => 1, 10 => 1 },
+          catalogue: catalogue
+          )]
+      end
+
+      let(:package) { OpenStruct.new(packs: packs) }
+
+      it 'has an order package' do
+        invoice = Invoicer.create(package)
+        expect(invoice.package).to
+      end
+      it 'should return total order value' do
+        invoice = Invoicer.create(package)
+        expect(invoice.total_value).to respond_to(19.98)
+      end
+      it 'should return breakdown of bundles' do
+        invoice = Invoicer.create(package)
+        details = "10 R12 $19.98\n\t1 x 5 $6.99\n\t1 x 10 $12.99"
+        expect(invoice.details).to eq(details)
+      end
+    end
+
+  #   context 'when multiple product order'
+  end
+end
