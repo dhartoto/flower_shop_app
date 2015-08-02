741cca8f343a80545097de4ff70fc769f97ed34a
Ardito Hartoto <dhartoto@gmail.com>
2015-08-02 10:55:32 +1000

    Fix bug in OrderValidator
    The algorithm for identifying if an order is fillable did not
    validate all cases. This bug was identified when a test was run
    for 11 Tulips, which can be made up of 1 x 5 and 2 x 3 bundles
    
    The new algorithm has the following steps:
    - Find all possible combinations that are less than or equal to
    the ordered amount. (#less_than_or_equal_to_combinations)
    - Find combinations that match the ordered amount, (#find_matches)
    - If matches exist then false is returned for #unfillable?
    
    The algorithm was also extracted to a Fillable module

---
+++ lib/fillable.rb
@@ -0,0 +1,60 @@
+module Fillable
+
+  def unfillable?(catalogue)
+    order_file = read_file
+    resp = false
+    order_file.each do |line|
+      line = line.first.split(' ')
+      order = line[0].to_i
+      product = catalogue.find(line[1])
+      bundles = product['bundles'].keys.sort { |x,y| x <=> y }
+      combinations = less_than_or_equal_to_combinations(bundles, order)
+      matches = find_matches(combinations, bundles, order)
+      resp = true if matches.empty?
+    end
+    resp
+  end
+
+  def find_matches(combinations, bundles, order)
+    match = []
+    combinations.each do |batch|
+      match << batch if total(batch, bundles) == order
+    end
+    match
+  end
+
+  def less_than_or_equal_to_combinations(bundles, order)
+    start_at = 0
+    end_at = bundles.size
+    counter = Array.new(bundles.size, 0)
+    possible_combinations = [counter.dup]
+    inner_counter = counter.dup
+
+    possible_combinations.each do |batch|
+      inner_counter = batch.dup
+      while start_at < end_at do
+        (start_at...end_at).each do |index|
+
+          while total(inner_counter, bundles) <= order do
+            possible_combinations << inner_counter.dup if total(inner_counter, bundles) <= order
+            inner_counter[index] += 1
+          end
+
+          inner_counter = batch.dup unless index == end_at and total(inner_counter, bundles) > order
+          possible_combinations.uniq!
+        end
+        start_at += 1
+      end
+      start_at = 1
+    end
+    possible_combinations.uniq
+  end
+
+  def total(batches, bundles)
+    total = 0
+    batches.each_with_index do |count, index|
+      total += bundles[index] * count
+    end
+    total
+  end
+end
