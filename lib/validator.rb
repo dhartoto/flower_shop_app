c36922c4db48ffa8c1243696494eb3631c12ac9d
Ardito Hartoto <dhartoto@gmail.com>
2016-09-10 16:12:20 +1000

    Update Validator implementation
    Refactored so that it operates like an organizer

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,33 +1,11 @@
 require_relative 'upload_validator'
 require_relative 'data_validator'
-require_relative 'order_validator'
 
 class Validator
-  VALIDATORS = [UploadValidator, DataValidator, OrderValidator]
-
   def self.validate(catalogue)
-    status = 200
-    error_message = nil
+    UploadValidator.validate
+    DataValidator.validate(catalogue)
 
-    VALIDATORS.each do |validator|
-      resp = validator.validate(catalogue)
-      if not resp.valid?
-        error_message = resp.error_message
-        status = 400
-        break
-      end
-    end
-    new(status: status, error_message: error_message)
-  end
-
-  attr_reader :status, :error_message
-
-  def initialize(options={})
-    @status = options[:status]
-    @error_message = options[:error_message]
-  end
-
-  def valid?
-    status == 200
+    true
   end
 end
--- spec/lib/validator_spec.rb
+++ spec/lib/validator_spec.rb
@@ -6,51 +6,35 @@ describe Validator do
   let(:catalogue) { Catalogue.create }
 
   context 'when no error' do
-    it 'is valid' do
-      validator = instance_double('Validator', valid?: true)
-      allow(UploadValidator).to receive(:validate) { validator }
-      allow(DataValidator).to receive(:validate) { validator }
-      allow(OrderValidator).to receive(:validate) { validator }
-      resp = Validator.validate(catalogue)
-      expect(resp.valid?).to eq(true)
+    before do
+      allow(UploadValidator).to receive(:validate)
+      allow(DataValidator).to receive(:validate)
+    end
+
+    it 'returns true' do
+      expect(Validator.validate(catalogue)).to eq(true)
     end
   end
 
-  let(:resp) { instance_double('Validator') }
-
   context 'when upload error' do
-    it 'return upload error message' do
-      allow(resp).to receive(:valid?).and_return(false, true, true)
-      allow(resp).to receive(:error_message) { 'file error' }
-      allow(UploadValidator).to receive(:validate) { resp }
-      allow(DataValidator).to receive(:validate) { resp }
-      allow(OrderValidator).to receive(:validate) { resp }
-      resp = Validator.validate(catalogue)
-      expect(resp.error_message).to eq('file error')
+    it 'raises file not found exception' do
+      allow(UploadValidator).to receive(:validate)
+        .and_raise(Application::FileError)
+      allow(DataValidator).to receive(:validate)
+
+      expect { Validator.validate(catalogue) }
+        .to raise_exception(Application::FileError)
     end
   end
 
   context 'when data error' do
     it 'return data error message' do
-      allow(resp).to receive(:valid?).and_return(true, false, true)
-      allow(resp).to receive(:error_message) { 'data error' }
-      allow(UploadValidator).to receive(:validate) { resp }
-      allow(DataValidator).to receive(:validate) { resp }
-      allow(OrderValidator).to receive(:validate) { resp }
-      resp = Validator.validate(catalogue)
-      expect(resp.error_message).to eq('data error')
-    end
-  end
+      allow(UploadValidator).to receive(:validate)
+      allow(DataValidator).to receive(:validate)
+        .and_raise(Application::DataError)
 
-  context 'when order error' do
-    it 'return order error message' do
-      allow(resp).to receive(:valid?).and_return(true, true, false)
-      allow(resp).to receive(:error_message) { 'order error' }
-      allow(UploadValidator).to receive(:validate) { resp }
-      allow(DataValidator).to receive(:validate) { resp }
-      allow(OrderValidator).to receive(:validate) { resp }
-      resp = Validator.validate(catalogue)
-      expect(resp.error_message).to eq('order error')
+      expect { Validator.validate(catalogue) }
+        .to raise_exception(Application::DataError)
     end
   end
 end
