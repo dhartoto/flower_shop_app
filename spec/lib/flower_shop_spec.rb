35e2b1e40fe5f61ba68cd1400ddd60e184ee1d2d
Ardito Hartoto <dhartoto@gmail.com>
2016-09-10 10:56:09 +1000

    Fix style linting in FlowerShop test

--- spec/lib/flower_shop_spec.rb
+++ spec/lib/flower_shop_spec.rb
@@ -11,18 +11,20 @@ describe FlowerShop do
       msg = " - Please save Order CSV file into the 'uploads' folder.\n"\
           " - Enter '1' to calculate costs and bundle breakdown\n"\
           " - Enter '2' to exit the Flower Shop App\n"
+
       expect(subject.response).to eq(msg)
     end
 
     it 'should have catalogue' do
       expect(Catalogue).to receive(:create)
+
       FlowerShop.new
     end
   end
 
   describe '#run' do
-    let(:app) { FlowerShop.new }
-    let(:order_engine) { instance_double('OrderEngine', response: 'some message') }
+    let(:stubbed_response) { 'some message' }
+    let(:order_engine) { instance_double('OrderEngine', response: stubbed_response) }
 
     before do
       allow(Catalogue).to receive(:create)
@@ -31,36 +33,46 @@ describe FlowerShop do
     end
 
     context 'when user enters 1 to calculate costs and bundle' do
-      before { allow(app).to receive(:gets) { '1' } }
+      before { allow(subject).to receive(:gets) { '1' } }
 
       it 'runs the order engine' do
         expect(order_engine).to receive(:run)
-        app.run
+
+        subject.run
       end
+
       it 'displays message to user' do
-        app.run
-        expect(app.response).to eq('some message')
+        subject.run
+
+        expect(subject.response).to eq(stubbed_response)
       end
     end
 
     context 'when user enters 2 to exit program' do
-      before { allow(app).to receive(:gets) { '2' } }
+      before { allow(subject).to receive(:gets) { '2' } }
 
       it 'does not fill order' do
         expect(OrderEngine).not_to receive(:new)
-        app.run
+
+        subject.run
       end
+
       it 'displays exit message to user' do
-        app.run
-        expect(app.response).to eq("Exiting the Flower Shop")
+        expected_response = "Exiting the Flower Shop"
+
+        subject.run
+
+        expect(subject.response).to eq(expected_response)
       end
     end
 
     context 'when user enters invalid entry' do
       it 'gets user input again' do
-        allow(app).to receive(:gets).and_return('wrong_input', '2')
-        expect(app).to receive(:gets).twice
-        app.run
+        allow(subject).to receive(:gets).and_return('wrong_input', '2')
+
+        expect(subject).to receive(:gets).twice
+
+        subject.run
       end
     end
   end
