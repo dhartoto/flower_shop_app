2291dfcaa510fb2d5f4dd50e5dd206e34c7731e3
Ardito Hartoto <dhartoto@gmail.com>
2016-09-10 14:19:33 +1000

    Update BaseValidator
    Delete redundant methods resulting from refactor of validators.

--- lib/base_validator.rb
+++ lib/base_validator.rb
@@ -4,20 +4,7 @@ class BaseValidator
   FILE_NAME = 'order.csv'
   FULL_PATH = "public/uploads/#{FILE_NAME}"
 
-  attr_reader :status, :error_message
-
-  def initialize(options={})
-    @status = options[:status]
-    @error_message = options[:error_message]
-  end
-
-  def valid?
-    status == 200
-  end
-
-  private
-
-  def self.read_file
+  def self.read_csv_file
     CSV.read(FULL_PATH)
   end
 end
